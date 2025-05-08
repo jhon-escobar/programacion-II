fc3023826ab7cbb76859c1465d7322d36993229e
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2025-05-08 18:06:28 -0400

    m

---
+++ p/esctructura de datos.py
@@ -0,0 +1,20 @@
+# pila = []
+# pila.append(1)
+# pila.pop()
+
+# class Pila:
+
+#     def __init__(self):
+#         self.item = []
+
+#     def push(self, item):
+#         return self.items.append()
+
+#     def pop(self, item):
+#         return self.items.pop()
+    
+#     def is_empty(self):
+#         # return len(item. items) == 0
+    
+    
+    
